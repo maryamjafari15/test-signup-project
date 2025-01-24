517fad240571c7eab0b83f759c98d51683a17927
maryam jafari <maryamjafari12175@gmail.com>
2025-01-24 18:47:50 +0330

    - install firebase as server and create slice redux

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxhkMfoqzkVnY4FMRgc5GfMingfGaLYY4",
+  authDomain: "test-formproject.firebaseapp.com",
+  databaseURL: "https://test-formproject-default-rtdb.firebaseio.com",
+  projectId: "test-formproject",
+  storageBucket: "test-formproject.firebasestorage.app",
+  messagingSenderId: "135895706306",
+  appId: "1:135895706306:web:5188f2b860d2502fe9a95b",
+  measurementId: "G-MLHQJHH4KT"
+};
+
+const app = initializeApp(firebaseConfig);
