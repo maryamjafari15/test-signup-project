8ef0c720b9f2f4d9503b349b265227dc13376912
maryam jafari <maryamjafari12175@gmail.com>
2025-01-24 14:31:30 +0330

    -home page and solve tailwind problem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,36 @@
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>سلاممم</div>
-  )
-}
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-cover bg-center bg-[url('/assets/bg.jpg')]">
+      <div className=' w-fit sm:w-3/4 md:w-1/2 h-1/2 sm:h-1/2 bg-white/50  rounded-3xl flex flex-col justify-center items-center p-6 sm:p-10'>
+        <h1 className='text-2xl sm:text-3xl font-semibold mb-6 sm:mb-10'>
+          {" "}
+          سلام ، خوش آمدید!{" "}
+        </h1>
+        <p className='text-sm sm:text-lg font-medium mb-6 sm:mb-9 text-center p-2'>
+          کاربر گرامی، در صورت عضویت در سایت بر روی دکمه <b> ورود</b> کلیک
+          نمایید ، در غیر اینصورت ابتدا میبایست در سایت عضو شوید!
+        </p>
+        <div className='flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-6 w-full '>
+          <button
+            className='p-3 sm:p-4 bg-blue-800 w-full sm:w-28 rounded-2xl text-sm sm:text-lg text-white hover:bg-blue-900 transition duration-300'
+            onClick={() => navigate("/LoginPage")}
+          >
+            ورود
+          </button>
+          <button
+            className='p-3 sm:p-4 bg-blue-400 w-full sm:w-28 rounded-2xl text-sm sm:text-lg text-white hover:bg-blue-500 transition duration-300'
+            onClick={() => navigate("/SignupPage")}
+          >
+            ثبت‌نام
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -7,4 +7,4 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-})
+});
