20c200046548abf848c9d475cdf8a4d2fe98068e
maryam jafari <maryamjafari12175@gmail.com>
2025-01-24 22:29:03 +0330

    deploy vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { PageNotFound } from "./components/PageNotFound";
-import '@fontsource-variable/vazirmatn';
+
 
 function App() {
   const router = createBrowserRouter([
